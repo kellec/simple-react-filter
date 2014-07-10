5b1270c81b93dc30553dcdf111c83bb5911a58e8
Kelle <kelle.connolly@gmail.com>
2014-07-11 08:59:35 +1200

    Make components own files.

--- src/App.js
+++ src/App.js
@@ -2,35 +2,14 @@
 'use strict';
 
 var React = require('react');
-var Panel = require('react-bootstrap/Panel');
 var Grid = require('react-bootstrap/Grid');
 var Row = require('react-bootstrap/Row');
 var Col = require('react-bootstrap/Col');
 var Well = require('react-bootstrap/Well');
 
+var FilterType = require('./FilterType');
 var data = require('./data');
 
-var Filter = React.createClass({
-  render: function() {
-    return (<li>{this.props.name}</li>);
-  }
-});
-
-var FilterType = React.createClass({
-  render: function() {
-    var title = (<h3>{this.props.title}</h3>);
-    return (
-      <Panel header={title}>
-        <ul>
-          {this.props.filterOptions.map(function(option) {
-            return <Filter name={option} />
-          })}
-        </ul>
-      </Panel>
-    );
-
-  }
-});
 
 var App = React.createClass({
   render: function() {
@@ -39,16 +18,16 @@ var App = React.createClass({
         <Row>
           <Col md={4} className="clearfix">
           <h2>Filters</h2>
-          {data.filters.map(function(filter) {
-            return (<FilterType title={filter.filterName} filterOptions={filter.filterOptions} />);
+          {data.filters.map(function(filter, index) {
+            return (<FilterType key={index} title={filter.filterName} filterOptions={filter.filterOptions} />);
           })}
           </Col>
           <Col md={8}>
             <h2>Teams</h2>
             <Well>
               <ul>
-                {data.content.map(function(item) {
-                  return(<li className={item.slug}>{item.name}</li>);
+                {data.content.map(function(item, index) {
+                  return(<li className={item.slug} key={index}>{item.name}</li>);
                 })}
               </ul>
             </Well>
---
+++ src/Filter.js
@@ -0,0 +1,26 @@
+/** @jsx React.DOM */
+'use strict';
+
+var React = require('react/addons');
+
+var Filter = React.createClass({
+  render: function() {
+    var classes = React.addons.classSet({
+      'open': this.props.active
+    });
+    var buttonClasses = React.addons.classSet({
+      'close': true,
+      'hidden': !this.props.active
+    });
+    return (
+      <li className={classes}>
+        <a>
+          {this.props.name}
+          <button type="button" className={buttonClasses}><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
+        </a>
+      </li>
+    );
+  }
+});
+
+module.exports = Filter;
